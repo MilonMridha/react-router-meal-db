82c28d1def6e619383ed68c303884bf466f93d9e
MilonMridha <milonmridha570@gmail.com>
2022-03-29 12:34:17 -0700

    update-routing

--- src/Components/Restaurent/Restaurent.js
+++ src/Components/Restaurent/Restaurent.js
@@ -21,13 +21,13 @@ const Restaurent = () => {
         <div className='container mt-5'>
             <h2 className='text-success'>Find the Food</h2>
           <input  onChange={searchFood} type="text" />
-          <h4 className='text-danger mt-3'>Found Item : {meals.length}</h4>
-          <div className=' container row row-cols-1 row-cols-md-3 justify-content-center g-4 mt-4'>
+          <h4 className='text-danger mt-3'>Found Item : {meals?.length}</h4>
+         {meals && <div className='container row row-cols-1 row-cols-md-3 justify-content-center g-4 mt-4'>
               {
              meals.map(meal => <Meal meal={meal}
              key ={meal.idMeal}></Meal>)
               }
-          </div>
+          </div>}
             
         </div>
     );
